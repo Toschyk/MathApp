5d3758a059fb2e1d7c6c4f8be2dc67c5ae4f7ec5
Toschyk <toschyk@icloud.com>
2024-11-17 21:47:53 +0700

    Прописан код NUnit

--- MathAppNUnitTests/UnitTest1.cs
+++ MathAppNUnitTests/UnitTest1.cs
@@ -1,16 +1,30 @@
+using NUnit.Framework;
+using MathApp;
+
 namespace MathAppNUnitTests
 {
-    public class Tests
+    [TestFixture]
+    public class CalculatorTests
     {
+        private Calculator calculator;
+
         [SetUp]
         public void Setup()
         {
+            calculator = new Calculator();
         }
 
         [Test]
-        public void Test1()
+        public void Add_TwoNumbers_ReturnsCorrectSum()
         {
-            Assert.Pass();
+            var result = calculator.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Divide_ByZero_ThrowsException()
+        {
+            Assert.Throws<DivideByZeroException>(() => calculator.Divide(10, 0));
         }
     }
 }
