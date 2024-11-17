7b7b7f9e4c850cea7eb93a087bb29543e6c01080
Toschyk <toschyk@icloud.com>
2024-11-17 21:38:00 +0700

    Написан код программы

--- MathApp/Program.cs
+++ MathApp/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace MathApp
 {
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Деление на ноль невозможно.");
+            return a / b;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите два числа:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            var calculator = new Calculator();
+            Console.WriteLine($"Сумма: {calculator.Add(a, b)}");
+            Console.ReadLine();
         }
     }
 }
