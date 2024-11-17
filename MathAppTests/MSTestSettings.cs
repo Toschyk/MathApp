050545683528164e5454c11cdfe9b166fde7b795
Toschyk <toschyk@icloud.com>
2024-11-17 21:45:39 +0700

    Прописан код MSTest

--- MathAppTests/MSTestSettings.cs
+++ MathAppTests/MSTestSettings.cs
@@ -1,1 +1,31 @@
-﻿[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathApp;
+
+namespace MathAppTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [TestMethod]
+        public void Add_TwoNumbers_ReturnsCorrectSum()
+        {
+            var result = calculator.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Divide_ByZero_ThrowsException()
+        {
+            calculator.Divide(10, 0);
+        }
+    }
+}
